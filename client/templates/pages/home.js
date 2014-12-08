bd2b482c83572032dac2c8ba07b442c491b5cf66
Mikhail Proniushkin <mikepro@Mikhails-MBP-4.home>
2014-12-08 02:35:34 -0500

    mixpanel events

--- client/templates/pages/home.js
+++ client/templates/pages/home.js
@@ -1,5 +1,20 @@
 Template.home.events({
-    'click .contact_me': function(e, tmpl) {
+    'click .social_link': function(e, tmpl) {
+        var url = $(e.target).closest('a').attr('href');
+        var parser = document.createElement('a');
+        parser.href = url
+
+        analytics.track({
+          event: 'Click Social Link',
+          url: parser.hostname
+        });
+    },
+
+    'click .email_link': function(e, tmpl) {
+        analytics.track({
+          userId: '12345',
+          event: 'Click email Link'
+        });
     }
 })
 
